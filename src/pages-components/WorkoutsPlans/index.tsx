1bf88c72c99ec73e20a237f74efc86458d5818fd
Bryan Martins <bryanmaraujo544@gmail.com>
2022-08-16 13:24:53 -0300

    feat: starting the workouts plans page

---
+++ src/pages-components/WorkoutsPlans/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+export const WorkoutsPlans = () => {
+  console.log('WorkoutsPlans');
+
+  return (
+    <Container>
+      <p>WorkoutsPlans</p>
+    </Container>
+  );
+};

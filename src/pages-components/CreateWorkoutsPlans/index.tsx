76529d70ac1514a7448e3772307377bd0e830fd9
Bryan Martins <bryanmaraujo544@gmail.com>
2022-08-22 17:10:28 -0300

    feat: adding background black alpha in suggestion workout card

--- src/pages-components/CreateWorkoutsPlans/index.tsx
+++ src/pages-components/CreateWorkoutsPlans/index.tsx
@@ -95,31 +95,33 @@ export const CreateWorkoutsPlans = () => {
               />
             </header>
             {/* Workouts Grid */}
-            <div className="grid grid-cols-2 gap-6 mt-10 max-h-[50rem] overflow-y-scroll">
+            <div className="grid grid-cols-2 gap-6 mt-10 max-h-[60rem] overflow-y-scroll">
               {FAKE_WORKOUTS.map((workout) => (
                 <div
                   key={workout.id}
-                  className="flex flex-col items-center gap-4 bg-blue-100 p-6 rounded-[1.6rem] bg-center"
+                  className="relative flex flex-col items-center gap-4 bg-blue-100 p-6 rounded-[1.6rem] bg-center overflow-hidden"
                   style={{
                     backgroundImage:
                       "url('https://i.pinimg.com/564x/b8/ea/06/b8ea0615898a93c0fd3907a07bbda69c.jpg')",
                   }}
                 >
-                  <span className="font-bold text-white text-3xl">
+                  <div className="absolute w-full h-full left-0 top-0 bg-[#00000050]" />
+                  {/* <div className='absolute w-full h-full top-0 '/> */}
+                  <span className="font-bold text-white text-3xl z-10">
                     {workout.title}
                   </span>
                   <div className="flex w-full gap-3">
-                    <div className="bg-blue-50 text-blue-500 font-bold text-center py-2 px-6 flex-1 rounded-2xl">
+                    <div className="bg-blue-50 text-blue-500 font-bold text-center py-2 px-6 flex-1 rounded-2xl z-10">
                       Sets: {workout.sets}
                     </div>
-                    <div className="bg-blue-50 text-blue-500 font-bold text-center py-2 px-6 flex-1 rounded-2xl">
+                    <div className="bg-blue-50 text-blue-500 font-bold text-center py-2 px-6 flex-1 rounded-2xl z-10">
                       Reps: {workout.reps}
                     </div>
                   </div>
                   <Button
                     type="button"
                     variant="outlined"
-                    className="w-full h-[3.6rem] mt-4"
+                    className="w-full h-[3.6rem] mt-4 z-10"
                   >
                     Adicionar
                   </Button>

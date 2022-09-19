ee4c432e6d05c2b7ffbeae7d8d2c60692e3e06dc
Bryan Martins <bryanmaraujo544@gmail.com>
2022-09-19 08:32:45 -0300

    fix: changing the api url

--- src/services/serverApi.ts
+++ src/services/serverApi.ts
@@ -4,5 +4,5 @@ const dev = 'http://localhost:8080';
 const prod = process.env.SERVER_API;
 
 export const serverApi = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? dev : prod,
+  baseURL: 'https://trainya-app-api.herokuapp.com/',
 });

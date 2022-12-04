8eb9528aa0bdd340c0d414a4241d2306843b2054
Julio Zambom <work.juliozambom@gmail.com>
2022-12-04 15:12:17 -0300

    changing api url

--- src/services/serverApi.ts
+++ src/services/serverApi.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const dev = 'http://localhost:8080';
 // const prod = 'https://trainya-app-p9dqv.ondigitalocean.app/';
-const prod = 'https://trainya-app-api.herokuapp.com/';
+// const prod = 'https://trainya-app-api.herokuapp.com/';
+const prod = 'https://trainya.up.railway.app';
 
 export const serverApi = axios.create({
   baseURL: prod,
